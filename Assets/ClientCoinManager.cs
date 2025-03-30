a28467185c47ec338726b556660160d2847af0de
TylerReeds <117316771+TylerReeds@users.noreply.github.com>
2025-03-30 13:44:53 -0400

    client and server connection fix

--- Assets/ClientCoinManager.cs
+++ Assets/ClientCoinManager.cs
@@ -17,7 +17,9 @@ public class ClientCoinManager : MonoBehaviour
     void Start()
     {
         UDPClient2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        UDPClient2.Bind(new IPEndPoint(IPAddress.Any, 8889)); // Assuming 8889 for UDP
+        UDPClient2.Bind(new IPEndPoint(IPAddress.Any, 0)); 
+
+        serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8889);
 
         // Start receiving coin data from the server
         UDPClient2.BeginReceiveFrom(UDPBuffer, 0, UDPBuffer.Length, 0, ref serverEndPoint, new AsyncCallback(ReceiveUDPCallback), UDPClient2);
